8c6bbe81155af1ce9001d4dca86411aff321359d
CooLNuanfeng <1634898698@qq.com>
2017-06-06 13:39:12 +0800

    update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ program.command('create <branch> <name>')
     .option('-v, --vue','是否使用vue')
     .description('在对应分支生成项目的初始文件')
     .action(function(branch,name,options){
-        if(tool.DirTools.isExist(filePath.CURRENT_PATH+'/'+branch) && tool.DirTools.isExist(filePath.CURRENT_PATH+'/static') ){
+        if(tool.FileTools.isExist(branch) && tool.FileTools.isExist('static') ){
             var config = {
                 'branch' : branch,
                 'name' : name,
@@ -32,7 +32,7 @@ program.command('create <branch> <name>')
 program.on('--help',function(){
     console.log('Example: ');
     console.log('');
-    console.log('  $ flstart huodong lilibao -v -s');
+    console.log('  $ flstart create huodong lilibao -v -s');
     console.log('');
 });
 
@@ -45,8 +45,7 @@ if(!program.args.length){
 }
 
 function createPhpTpl(conf){
-    tool.DirTools.isExist(conf.path+'/'+conf.branch+'/Tpl/default') &&
-    tool.DirTools.mkDir(conf.path+'/'+conf.branch+'/Tpl/default/'+conf.name);
+    tool.FileTools.mkdirSync(conf.branch+'/Tpl/default/'+conf.name);
     var htmlContent = '';
     if(conf.vue){
         htmlContent = tool.FileTools.readFile(filePath.TPL_PATH+'/tpl-vue.html').toString();
@@ -54,7 +53,7 @@ function createPhpTpl(conf){
         htmlContent = tool.FileTools.readFile(filePath.TPL_PATH+'/tpl.html').toString();
     }
 
-    tool.FileTools.createFile(conf.path+'/'+conf.branch+'/Tpl/default/'+conf.name+'/index.html',htmlContent);
+    tool.FileTools.createFile(conf.branch+'/Tpl/default/'+conf.name+'/index.html',htmlContent);
 }
 
 
@@ -66,41 +65,40 @@ function createStatic(conf){
     var scssContent = tool.FileTools.readFile(filePath.TPL_PATH+'/style.scss').toString();
 
     if(conf.branch == 'huodong'){
-        tool.DirTools.isExist(conf.path+'/static/'+conf.branch) && tool.DirTools.mkDir(conf.path+'/static/'+conf.branch+'/'+conf.name)
-
-        tool.DirTools.mkDir(conf.path+'/static/'+conf.branch+'/'+conf.name+'/css');
-        tool.DirTools.mkDir(conf.path+'/static/'+conf.branch+'/'+conf.name+'/js');
-        tool.DirTools.mkDir(conf.path+'/static/'+conf.branch+'/'+conf.name+'/images');
+        var staticPath = 'static/'+conf.branch+'/'+conf.name;
+        tool.FileTools.mkdirSync(staticPath+'/css');
+        tool.FileTools.mkdirSync(staticPath+'/js');
+        tool.FileTools.mkdirSync(staticPath+'/images');
 
         if(conf.sass){
-            tool.FileTools.createFile(conf.path+'/static/'+conf.branch+'/'+conf.name+'/css/'+conf.name+'.scss',scssContent);
+            tool.FileTools.createFile(staticPath+'/css/'+conf.name+'.scss',scssContent);
         }else{
-            tool.FileTools.createFile(conf.path+'/static/'+conf.branch+'/'+conf.name+'/css/'+conf.name+'.css',cssContent);
+            tool.FileTools.createFile(staticPath+'/css/'+conf.name+'.css',cssContent);
         }
 
         if(conf.vue){
-            tool.FileTools.createFile(conf.path+'/static/'+conf.branch+'/'+conf.name+'/js/'+conf.name+'.js',vuejsContent);
+            tool.FileTools.createFile(staticPath+'/js/'+conf.name+'.js',vuejsContent);
         }else{
-            tool.FileTools.createFile(conf.path+'/static/'+conf.branch+'/'+conf.name+'/js/'+conf.name+'.js',jsContent);
+            tool.FileTools.createFile(staticPath+'/js/'+conf.name+'.js',jsContent);
         }
 
     }else{
         var file = conf.branch == 'mobile' ? 'webapp' : conf.branch;
-        tool.DirTools.isExist(conf.path+'/static/'+file+'/css') &&
-        tool.DirTools.mkDir(conf.path+'/static/'+file+'/css/'+conf.name);
-        tool.DirTools.isExist(conf.path+'/static/'+file+'/js') && tool.DirTools.mkDir(conf.path+'/static/'+file+'/js/'+conf.name);
-        tool.DirTools.isExist(conf.path+'/static/'+file+'/images') && tool.DirTools.mkDir(conf.path+'/static/'+file+'/images/'+conf.name);
+        var staticPath = 'static/'+file;
+        tool.FileTools.mkdirSync(staticPath+'/css/'+conf.name);
+        tool.FileTools.mkdirSync(staticPath+'/js/'+conf.name);
+        tool.FileTools.mkdirSync(staticPath+'/images/'+conf.name);
 
         if(conf.sass){
-            tool.FileTools.createFile(conf.path+'/static/'+file+'/css/'+conf.name+'/'+conf.name+'.scss',scssContent);
+            tool.FileTools.createFile(staticPath+'/css/'+conf.name+'/'+conf.name+'.scss',scssContent);
         }else{
-            tool.FileTools.createFile(conf.path+'/static/'+file+'/css/'+conf.name+'/'+conf.name+'.css',cssContent);
+            tool.FileTools.createFile(staticPath+'/css/'+conf.name+'/'+conf.name+'.css',cssContent);
         }
 
         if(conf.vue){
-            tool.FileTools.createFile(conf.path+'/static/'+file+'/js/'+conf.name+'/'+conf.name+'.js',vuejsContent);
+            tool.FileTools.createFile(staticPath+'/js/'+conf.name+'/'+conf.name+'.js',vuejsContent);
         }else{
-            tool.FileTools.createFile(conf.path+'/static/'+file+'/js/'+conf.name+'/'+conf.name+'.js',jsContent);
+            tool.FileTools.createFile(staticPath+'/js/'+conf.name+'/'+conf.name+'.js',jsContent);
         }
     }
 }
--- tool/tool.js
+++ tool/tool.js
@@ -1,70 +1,72 @@
 const fs = require('fs');
 
-
 var FileTools = {
-  // 判断文件是否存在
+  // 判断文件或文件夹是否存在
   isExist: function(path) {
-    return fs.existsSync(path);
-  },
-  // 删除文件
-  rmFile: function(path) {
-    if (FileTools.isExist(path)) {
-      return fs.unlinkSync(path);
-    } else {
-      console.log('该文件不存在');
-      return null;
-    }
-  },
-  // 新建／覆盖／修改 原文件内容
-  createFile: function(path, data) {
-    var res = fs.writeFileSync(path, data);
-    return res;
-  },
-  // 读文件
-  readFile: function(path) {
-    if (FileTools.isExist(path)) {
-      var data = fs.readFileSync(path);
-      return data;
-    } else {
-      return null;
-    }
-  },
-  // 关闭文件
-  closeFile: function() {
-    fs.closeSync();
-  }
-};
-
-var DirTools = {
-  // 判断文件夹是否存在
-  isExist: function(path) {
-     return fs.existsSync(path);
+    return fs.existsSync(path) ? true : false;
   },
   // 创建文件夹
-  mkDir: function(path) {
-    if (!DirTools.isExist(path)){
-      return fs.mkdirSync(path);
+  mkdirSync: function(path, mode) {
+    var arr = path.split("/"), mode = mode || 0755;
+    if(arr[0]==='.'){
+        arr.shift();
+    }
+    if(arr[0]==='..'){
+        arr.splice(0,2,arr[0]+'/'+arr[1]);
+    }
+    function mk(cur){
+       if(!FileTools.isExist(cur)){//不存在就创建一个
+           fs.mkdirSync(cur, mode)
+       }
+       if(arr.length){
+           mk(cur + "/"+arr.shift());
+       }
+    }
+    arr.length && mk(arr.shift());
+  },
+  //创建文件
+  createFile : function(path,data){
+      return fs.writeFileSync(path,data);
+  },
+  //读取文件
+  readFile : function(path){
+    if (FileTools.isExist(path)) {
+      return fs.readFileSync(path);
     } else {
-      return false;
+      return null;
     }
   },
-  //删除文件夹
-  rmDir: function(path) {
-    if (DirTools.isExist(path)) {
-      return fs.rmdirSync(path);
-    } else {
-      console.log('该文件夹不存在');
-    }
+  // 删除文件或文件夹
+  rmFile: function(path) {
+      var dirs = [];
+       try{
+           iterator(path,dirs);
+           for(var i = 0, el ; el = dirs[i++];){
+               fs.rmdirSync(el);//一次性删除所有收集到的目录
+           }
+       }catch(e){//如果文件或目录本来就不存在，fs.statSync会报错，不过我们还是当成没有异常发生
+           console.log(e);
+       }
+      function iterator(url,dirs){
+          var stat = fs.statSync(url);
+          if(stat.isDirectory()){
+              dirs.unshift(url);//收集目录
+              inner(url,dirs);
+          }else if(stat.isFile()){
+              fs.unlinkSync(url);//直接删除文件
+          }
+      }
+      function inner(path,dirs){
+          var arr = fs.readdirSync(path);
+          for(var i = 0, el ; el = arr[i++];){
+              iterator(path+"/"+el,dirs);
+          }
+      }
   },
-  // 读取文件夹下所有文件
-  readDir: function(path) {
-    var data = fs.readdirSync(path);
-    return data;
-  }
+
 };
 
 
 module.exports = {
-    DirTools :  DirTools,
     FileTools : FileTools
 }
